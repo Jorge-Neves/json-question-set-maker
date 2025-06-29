490edd799250a48bb96604228450f2c1ae54dc68
Paulo Santos <paulo.jorge.ngs@gmail.com>
2025-06-29 15:55:27 +0100

    write logic to parse text and split by empty line breaks

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 const fs = require("fs");
 
 const data = fs.readFileSync("input.txt", "utf8");
-console.log(data);
+
+// Normalize all line endings first
+const normalizedData = data.replace(/\r\n?/g, "\n");
+
+// Now split on blank lines
+const textLines = normalizedData.split(/\n\s*\n/);
+
+// Trim each block
+const cleanTextLines = textLines.map((line) => line.trim());
+
+console.log("cleanTextLines", cleanTextLines);
+
+console.log("type of variable data", typeof data);
+
+console.log("type of variable textLines:", typeof textLines);
+
+console.log("data raw:", data);
+
+console.log("textLines", textLines);
+
+console.log("cleanTextLines", cleanTextLines);
+
+console.log("line break?:", cleanTextLines[2]);
